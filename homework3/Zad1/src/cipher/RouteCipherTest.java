7bbbe4b1689cc39f7d00be17d7957bb7f48bcd8b
Georgi Tsekov <72854235+Georgi1212@users.noreply.github.com>
2022-05-23 22:13:18 +0300

    Update RouteCipherTest.java

--- homework3/Zad1/src/cipher/RouteCipherTest.java
+++ homework3/Zad1/src/cipher/RouteCipherTest.java
@@ -2,6 +2,35 @@ package cipher;
 
 public class RouteCipherTest {
     public static void main(String[] args) {
+RouteCipher routeCipher = new RouteCipher(4);
+        String text = "THIS IS THE PLAIN TEXT";
+        String encrypted = routeCipher.encrypt(text);
+        System.out.println("Plain text: " + text);
+        System.out.println("Encrypted text is: " + encrypted);
 
+        String decrypted = routeCipher.decrypt(encrypted);
+        System.out.println("Decrypted text is: " + decrypted);
+
+        System.out.println();
+
+        routeCipher.setKey(5);
+        text = "ABORT THE MISSION, YOU HAVE BEEN SPOTTED";
+        encrypted = routeCipher.encrypt(text);
+        System.out.println("Plain text: " + text);
+        System.out.println("Encrypted text is: " + encrypted);
+
+        decrypted = routeCipher.decrypt(encrypted);
+        System.out.println("Decrypted text is: " + decrypted);
+
+        System.out.println();
+
+        routeCipher.setKey(-5);
+        text = "ABORT THE MISSION, YOU HAVE BEEN SPOTTED";
+        encrypted = routeCipher.encrypt(text);
+        System.out.println("Plain text: " + text);
+        System.out.println("Encrypted text is: " + encrypted);
+
+        decrypted = routeCipher.decrypt(encrypted);
+        System.out.println("Decrypted text is: " + decrypted);
     }
 }
